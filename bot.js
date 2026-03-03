07faa7338af68b7532564942282bbb1c20a8888a
MrZozolek <jakubcipinski@gmail.com>
2026-03-04 02:09:20 +0630

    Update bot.js with new configuration and logic

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const https = require('https');
 const { URL } = require('url');
 
 // ===== KONFIGURACJA =====
-const BOT_TOKEN = 'TWOJ_TOKEN_BOTA';
+const BOT_TOKEN = 'MTQ3NjAwMzcwODEwNTEzNDExMA.GMAQzT.zgWQsOSuVqFy3_oL6jEQlYNeA5Muk3A7BCminc';
 const PREFIX = '.';
 // ========================
 
@@ -201,3 +201,4 @@ client.once('ready', () => {
 });
 
 client.login(BOT_TOKEN);
+
